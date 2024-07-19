916b1c2e91caa30ea4fb9575030412dca9ffbccb
KatsuKira <christopher.caysido@gmail.com>
2024-07-20 01:54:58 +0800

    Created the create_array function
    the functions iterates through each word in an array and proceeds to check if item is included in the dictionary array

--- substring.rb
+++ substring.rb
@@ -2,16 +2,30 @@
 #and then an array of valid substrings (your dictionary) as the second argument. 
 # It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 def substrings(string,dictionary)
-  words = str_array(string)
-  
-  create_hash()
+  words = str_to_array(string)
+  selected_words = create_array(words,dictionary)
+  create_hash(selected_words)
 end
 
 def str_to_array(string)
-  string.tr('^a-zA-Z0-9 ','').split(" ")
+  string.tr('^a-zA-Z0-9 ','').downcase.split(" ")
 end
 
+def create_array(words,dictionary)
+  # debugger
+  to_hash = []
+  words.each do |word|
+    dictionary.each do |dict_entry|
+      if word.include?(dict_entry) 
+        to_hash.push(dict_entry)
+      end
+    end
+  end
+  to_hash
+end
 
 def create_hash(array)
   array.reduce(Hash.new(0)) do |subset, item|
@@ -19,4 +33,7 @@ def create_hash(array)
     subset
   end
 end
+
+puts substrings("Howdy partner, sit down! How's it going?",dictionary)
+
 
