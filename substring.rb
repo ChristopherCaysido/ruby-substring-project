646411a63ea1eae14348705fad22365b04e5e8f5
KatsuKira <christopher.caysido@gmail.com>
2024-07-19 18:16:23 +0800

    Created str_to_array function
    This function first strips the statement of all symbols and then
    uses the split method to create the
    array

---
+++ substring.rb
@@ -0,0 +1,12 @@
+# Implement a method #substrings that takes a word as the first argument 
+#and then an array of valid substrings (your dictionary) as the second argument. 
+# It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+
+def substrings(string,dictionary)
+  words = str_array(string)
+end
+
+def str_to_array(string)
+  string.tr('^a-zA-Z0-9 ','').split(" ")
+end
+
